cba00a8636918ab308a29547268b752fde6d9a72
PattanasakGit <111237717+PattanasakGit@users.noreply.github.com>
2022-10-11 16:59:45 +0700

    Navbar_OK

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+//import Users from "./components/Users";
+//import UserCreate from "./components/UserCreate";
+export default function App() {
+return (
+  <Router>
+   <div>
+   <Navbar />
+   {/* <Routes>
+       <Route path="/" element={<Users />} />
+       <Route path="/create" element={<UserCreate />} />
+   </Routes> */}
+   </div>
+  </Router>
+);
 }
-
-export default App;
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,62 @@
+// import * as React from "react";
+// import AppBar from "@mui/material/AppBar";
+// import Box from "@mui/material/Box";
+// import Toolbar from "@mui/material/Toolbar";
+// import Typography from "@mui/material/Typography";
+// import IconButton from "@mui/material/IconButton";
+// import MenuIcon from "@mui/icons-material/Menu";
+// function Navbar() {
+//  return (
+//    <Box sx={{ flexGrow: 1 }}>
+//      <AppBar position="static">
+//        <Toolbar>
+//          <IconButton
+//            size="large"
+//            edge="start"
+//            color="inherit"
+//            aria-label="menu"
+//            sx={{ mr: 2 }}
+//          >
+//            <MenuIcon />
+//          </IconButton>
+//          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+//            System Analysis and Design Pattanasak Test
+//          </Typography>
+//        </Toolbar>
+//      </AppBar>
+//    </Box>
+//  );
+
+// }
+
+// export default Navbar;
+
+
+
+
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function DenseAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="secondary">
+        <Toolbar variant="dense">
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          
+          <Typography variant="h6" color="inherit" component="div">
+            Pattanasak Atakul
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
