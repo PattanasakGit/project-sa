8776160c6519f0b5800ba1191654794a932bb902
PattanasakGit <111237717+PattanasakGit@users.noreply.github.com>
2022-10-11 14:30:09 +0700

    firt edit

---
+++ backend/entity/setup.go
@@ -0,0 +1,31 @@
+package entity
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func DB() *gorm.DB {
+	return db
+}
+func SetupDatabase() {
+	database, err := gorm.Open(sqlite.Open("sa-65-week5.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	database.AutoMigrate(
+		&User{},
+		&Gender{},
+		&Blood_type{},
+		&Drug_Allergy{},
+		&RIGHTS{},
+		&TEST{},
+		&Patient{},
+	)
+
+	db = database
+}
